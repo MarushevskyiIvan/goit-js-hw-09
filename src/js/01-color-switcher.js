c357ddc32219956f920ac6b46fbb5a0f458b26fd
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2023-09-17 00:17:25 +0300

    9.1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stoptBtnEl = document.querySelector('[data-stop]');
 
+startBtnEl.addEventListener('click', startChangeColor);
+
+stoptBtnEl.setAttribute('disabled', true);
+
+function startChangeColor() {
+  startBtnEl.setAttribute('disabled', true);
+  stoptBtnEl.removeAttribute('disabled');
+  const changeColorInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  stoptBtnEl.addEventListener('click', stopChangeColor);
+  function stopChangeColor() {
+    startBtnEl.removeAttribute('disabled');
+    stoptBtnEl.setAttribute('disabled', true);
+
+    clearInterval(changeColorInterval);
+  }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
