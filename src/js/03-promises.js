29af63ea6331097599c9e1d0e6fbe184ec6e453a
Marushevskyi Ivan <131617971+MarushevskyiIvan@users.noreply.github.com>
2023-09-19 22:35:21 +0300

    9.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,31 +1,48 @@
 const formEl = document.querySelector('.form');
 const btnEl = document.querySelector('.form button');
 const inputEl = document.querySelector('input');
-console.log(btnEl);
 
-function createPromise(position, delay) {
-  return new Promise((res, rej) => {
-    const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve();
-    } else {
-      reject();
+formEl.addEventListener('submit', onFormSubmit);
+let position = 0;
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const formData = {
+    delay: formEl.elements.delay.value,
+    step: formEl.elements.step.value,
+    amount: formEl.elements.amount.value,
+  };
+  // console.log(formData);
+
+  const delay = Number(formData.delay) + Number(formData.step);
+  console.log(delay);
+
+  const intervalId = setInterval(() => {
+    function createPromise(position, delay) {
+      if (position === Number(formData.amount)) {
+        clearInterval(intervalId);
+        return;
+      }
+
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          const shouldResolve = Math.random() > 0.3;
+          if (shouldResolve) {
+            resolve({ position, delay });
+          } else {
+            reject({ position, delay });
+          }
+        }, delay);
+      });
     }
-  });
+    position += 1;
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }, delay);
 }
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-const formData = {};
-
-btnEl.addEventListener('submit', function asa(evt) {
-  evt.preventDefault();
-  const form = evt.currentTarget;
-  const login = form.elements.delay.value;
-  console.log('привет');
-});
+function intervalId() {}
